4586b132f70a71d24d225b485de0dd425bc82d94
Saad T <tariqs26@mcmaster.ca>
2022-10-06 19:27:24 -0400

    fix error with rule, add tech icons, change page icon, make overall transition faster

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,24 +1,45 @@
 import React from "react";
 import Section from "../Section/Section";
+import iconLinks from "./iconLinks";
+import "./About.css";
 
 export default function About() {
   return (
     <Section heading={"About me"}>
       <p>Hi There!</p>
       <p>
-        I'm Saad Tariq, a computer science student at
-        McMaster University in my second year. I have
-        experience with front-end web development but I'am looking to expand my
-        knowledge towards back-end development.
+        I'm Saad Tariq, a computer science student at McMaster University in my
+        second year. I have experience with front-end web development but I'am
+        looking to expand my knowledge towards back-end development.
       </p>
       <p>
         Some of my favorite projects include this website a trivia app and a
         full-stack camp site review app.
       </p>
-      <p>
-        I have experience with <code>HTML, CSS, Javascript, React, Node.js, Express,
-        MongoDB, and Git.</code>
-      </p>
+      <p>Here are some technologies I use in my projects:</p>
+      <div className="tech-icons">
+        {[
+          "HTML",
+          "CSS",
+          "JavaScript",
+          "TypeScript",
+          "React",
+          "Redux",
+          "Node",
+          "Python",
+        ].map((name) => {
+          return <TechIcon key={name} icon={iconLinks[name]} name={name} />;
+        })}
+      </div>
     </Section>
   );
 }
+
+const TechIcon = ({ icon, name }) => {
+  return (
+    <div className="tech-icon">
+      <img src={icon} alt={name} />
+      <span>{name}</span>
+    </div>
+  );
+};
---
+++ src/components/About/iconLinks.js
@@ -0,0 +1,25 @@
+const baseURL = "https://img.icons8.com",
+  sz = "40",
+  opt = "000000",
+  iconBase = `${baseURL}/color/${sz}/${opt}`;
+
+const iconLinks = {
+  MongoDB: `${baseURL}/external-tal-revivo-shadow-tal-revivo/${sz}/external-mongodb-a-cross-platform-document-oriented-database-program-logo-shadow-tal-revivo.png`,
+  Node: `${iconBase}/nodejs.png`,
+  React: `${iconBase}/react-native.png`,
+  Redux: `${iconBase}/redux.png`,
+  Express: `${iconBase}/express.png`,
+  HTML: `${iconBase}/html-5.png`,
+  CSS: `${iconBase}/css3.png`,
+  JavaScript: `${iconBase}/javascript.png`,
+  TypeScript: `${iconBase}/typescript.png`,
+  Git: `${iconBase}/git.png`,
+  Github: `${iconBase}/github.png`,
+  npm: `${iconBase}/npm.png`,
+  Python: `${iconBase}/python.png`,
+  Java: `${iconBase}/java-coffee-cup-logo.png`,
+  heroku: `${iconBase}/heroku.png`,
+  netlify: `${iconBase}/netlify.png`,
+};
+
+export default iconLinks;
