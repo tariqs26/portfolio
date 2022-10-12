13edc359aa76853be5da938fb42a955da60c1e00
Saad T <tariqs26@mcmaster.ca>
2022-10-11 20:03:25 -0400

    update units to rem, reduce nav height when scrolled and increase the threshold for detection

--- src/hooks/useNavShadow.ts
+++ src/hooks/useNavShadow.ts
@@ -4,7 +4,7 @@ export default function useNavShadow() {
   const [pos, setPos] = useState(0);
   useEffect(() => {
     const handleScroll = () => {
-      (pos === 0 || window.scrollY === 0) && setPos(window.scrollY);
+      (pos <= 20 || window.scrollY <= 20) && setPos(window.scrollY);
     };
     window.addEventListener("scroll", handleScroll);
     return () => {
@@ -13,7 +13,7 @@ export default function useNavShadow() {
   }, [pos]);
 
   useEffect(() => {    
-    if (pos > 0) document.querySelector(".nav")?.classList.add("nav-shadow");
+    if (pos >= 20) document.querySelector(".nav")?.classList.add("nav-shadow");
     else document.querySelector(".nav")?.classList.remove("nav-shadow");
   }, [pos]);
 } 
