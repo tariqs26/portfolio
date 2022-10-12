ea8bbee1ea03fa177fd1abc38f933dd19da5f69f
Saad T <tariqs26@mcmaster.ca>
2022-10-11 20:37:06 -0400

    change font, update media quries, make better contrasting color choices

--- src/hooks/useNavShadow.ts
+++ src/hooks/useNavShadow.ts
@@ -4,7 +4,7 @@ export default function useNavShadow() {
   const [pos, setPos] = useState(0);
   useEffect(() => {
     const handleScroll = () => {
-      (pos <= 20 || window.scrollY <= 20) && setPos(window.scrollY);
+      (pos < 20 || window.scrollY < 20) && setPos(window.scrollY);
     };
     window.addEventListener("scroll", handleScroll);
     return () => {
@@ -12,8 +12,8 @@ export default function useNavShadow() {
     };
   }, [pos]);
 
-  useEffect(() => {    
-    if (pos >= 20) document.querySelector(".nav")?.classList.add("nav-shadow");
+  useEffect(() => {        
+    if (pos > 19) document.querySelector(".nav")?.classList.add("nav-shadow");
     else document.querySelector(".nav")?.classList.remove("nav-shadow");
   }, [pos]);
 } 
