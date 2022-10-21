ffd450da38c43cc416c81698541c01289a0735c4
Saad T <tariqs26@mcmaster.ca>
2022-10-21 00:05:27 -0400

    change contact page content, and padding top of the intro

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import Section from '../Section/Section';
+import Link from '../Link/Link';
 
 export default function Contact() {
   return (
     <Section heading='Contact'>
       <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit,
-        voluptatum minus. Provident accusamus fugiat, harum laboriosam eveniet,
-        nam corporis ducimus sequi, quam explicabo quae vel laudantium ex ut
-        nemo sapiente!
+        Reach out to me on{' '}
+        <Link
+          href='https://twitter.com/Linklexandereardon'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          linkedin
+        </Link>
+        , or shoot me an{'\n'}
+        <Link href='mailto:' target='_blank' rel='noopener noreferrer'>
+          email
+        </Link>
       </p>
     </Section>
   );
