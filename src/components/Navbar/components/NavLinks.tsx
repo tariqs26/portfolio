3304592abc2113def40c52391acd6a01aedc6a5f
Saad T <tariqs26@mcmaster.ca>
2022-10-20 23:44:23 -0400

    refactoring css, make change to section heading in the hidden state to have no filter, as this was causing issues with shifting

--- src/components/Navbar/components/NavLinks.tsx
+++ src/components/Navbar/components/NavLinks.tsx
@@ -3,22 +3,16 @@ import './NavLinks.css';
 
 export default function NavLinks() {
   return (
-    <ul className='nav-links'>
-      <li className='nav-link'>
-        <a href='#about' className='link'>
-          About
-        </a>
-      </li>
-      <li className='nav-link'>
-        <a href='#projects' className='link'>
-          Projects
-        </a>
-      </li>
-      <li className='nav-link'>
-        <a href='#contact' className='link'>
-          Contact
-        </a>
-      </li>
-    </ul>
+    <div className='nav-links'>
+      <a href='#about' className='link'>
+        About
+      </a>
+      <a href='#projects' className='link'>
+        Projects
+      </a>
+      <a href='#contact' className='link'>
+        Contact
+      </a>
+    </div>
   );
 }
