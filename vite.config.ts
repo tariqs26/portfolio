80ab6cec6454c70e94d82b462a094dcdcf4a4b5e
Saad T <tariqs26@mcmaster.ca>
2022-11-23 18:07:36 -0500

    update host to localhost and port to 3000, in vire config file

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,8 @@ import svgrPlugin from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+  server: {
+    host: 'localhost',
+    port: 3000,
+  },
 });
