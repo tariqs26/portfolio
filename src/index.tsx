ad2b1b689bc3cf10514ae742c04aae503079907d
Saad T <tariqs26@mcmaster.ca>
2022-11-29 23:36:32 -0500

    update colors, add strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,11 @@
 import { StrictMode } from 'react';
-import * as ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  // <React.StrictMode>
-  <App />
-  // </React.StrictMode>
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
