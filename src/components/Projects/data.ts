4d91a4a38a0606342242ba8942f5b4d721664536
Saad T <tariqs26@mcmaster.ca>
2022-11-30 14:55:18 -0500

    create functions to generate links, given the project name

--- src/components/Projects/data.ts
+++ src/components/Projects/data.ts
@@ -1,16 +1,16 @@
-const start = "https://"
-const GitHub = `${start}github.com/tariqs26`;
-const Netlify = 'netlify.app';
-const imgPath = "./src/assets"
+const start = 'https://';
+const gitHub = (name: string) => `${start}github.com/${name}`;
+const netlify = (name: string) => `${start}${name}netlify.app`;
+const imgPath = './src/assets';
 
 const projects = [
   {
     name: 'Quizr',
     desc: 'A trivia app built with React and Typescript, using the Open Trivia Database API. Deployed on Netlify.',
     tech: ['React', 'Redux'],
-    imgSrc: 'https://wallpaper.dog/large/20359633.jpg',
-    liveLink: `${start}quizr.${Netlify}`,
-    codeLink: `${GitHub}/React-Quiz-App`,
+    imgSrc: '${imgPath}/project1.png',
+    liveLink: netlify('quizr'),
+    codeLink: gitHub('quizr'),
   },
 
   {
@@ -18,17 +18,17 @@ const projects = [
     desc: 'An application to manage job applications, filter by status, add files, notes, and more.',
     tech: ['Next.js', 'Typescript', 'PostgreSQL', 'Prisma'],
     imgSrc: `${imgPath}/project1.png`,
-    liveLink: `$start}pathfinder.${Netlify}`,
-    codeLink: `${GitHub}/Path-Finder`,
+    liveLink: netlify('job-tracker'),
+    codeLink: gitHub('job-tracker'),
   },
 
   {
     name: 'YelpCamp',
     desc: 'A campground review app',
     tech: ['Node', 'Express', 'MongoDB'],
-    imgSrc: 'https://wallpaper.dog/large/20359633.jpg',
-    liveLink: `${start}yelpcamp.${Netlify}`,
-    codeLink: `${GitHub}/YelpCamp`,
+    imgSrc: '${imgPath}/project1.png',
+    liveLink: netlify('yelpcamp'),
+    codeLink: gitHub('yelpcamp'),
   },
 ];
 
