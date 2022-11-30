46b0375a6582673e25ecc2bbb85325c1b85127da
Saad T <tariqs26@mcmaster.ca>
2022-11-30 00:13:46 -0500

    implement project feature successfuly

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,31 +1,36 @@
 import Section from '../Section/Section';
 import projects from './data';
-import { GitHubIcon, LinkedInIcon } from 'components/Icons';
+import { GitHubIcon, LiveLinkIcon } from 'components/Icons';
 import './Projects.css';
 
 export default function Projects() {
   return (
-    <Section heading={'Projects'}>
-      <div className='projects'>
-        {projects.slice(0,2).map((project) => (
-          <div className='project' key={project.title}>
+    <>
+      <Section heading={'Projects'}>{null}</Section>
+      {projects.map((project) => (
+        <div className='project-container section-container'>
+          <div className='project section hidden' key={project.name}>
             <img className='project-img' src={project.image} alt='' />
             <div className='project-content'>
-              <h3 className='project-name'>{project.title}</h3>
-              <p className='project-desc'>{project.description}</p>
+              <h3 className='project-name'>{project.name}</h3>
               <div className='project-tech-container'>
-                {project.technologies.map((tech) => (
+                {project.tech.map((tech) => (
                   <code key={tech}>{tech}</code>
                 ))}
               </div>
+              <p className='project-desc'>{project.description}</p>
               <div className='project-links'>
-                <a href={project.githubLink}><GitHubIcon /></a>
-                <a href={project.liveLink}><LinkedInIcon /></a>
+                <a href={project.codeLink}>
+                  <GitHubIcon />
+                </a>
+                <a href={project.liveLink}>
+                  <LiveLinkIcon />
+                </a>
               </div>
             </div>
           </div>
-        ))}
-      </div>
-    </Section>
+        </div>
+      ))}
+    </>
   );
 }
--- src/components/Projects/data.ts
+++ src/components/Projects/data.ts
@@ -3,42 +3,30 @@ const Netlify = 'netlify.app';
 
 const projects = [
   {
-    title: 'Quizr',
+    name: 'Quizr',
     description: 'A trivia app built with React and Typescript, using the Open Trivia Database API. Deployed on Netlify.',
-    features: [
-      'create quiz, based on data from the Open Trivia Database',
-      'a timed quiz with a score',
-    ],
-    technologies: ['React', 'Redux'],
+    tech: ['React', 'Redux'],
     image: 'https://wallpaper.dog/large/20359633.jpg',
     liveLink: `quizr.${Netlify}`,
-    githubLink: `${GitHub}/React-Quiz-App`,
+    codeLink: `${GitHub}/React-Quiz-App`,
   },
 
   {
-    title: 'Job Tracker',
-    description: 'A job application tracker built with next.js, Typescript, PostgreSQL, and Prisma. Deployed on Vercel. Manage applications, add notes, and track the status',
-    features: [
-      'visualize the A* path finding algorithm',
-      "visualize the Dijkstra's algorithm",
-    ],
-    technologies: ['Next.js', 'Typescript', 'PostgreSQL', 'Prisma'],
-    image: 'https://wallpaper.dog/large/20359633.jpg',
+    name: 'Job Tracker',
+    description: 'An application to manage job applications, filter by status, add files, notes, and more.',
+    tech: ['Next.js', 'Typescript', 'PostgreSQL', 'Prisma'],
+    image: './public/project1.png',
     liveLink: `pathfinder.${Netlify}`,
-    githubLink: `${GitHub}/Path-Finder`,
+    codeLink: `${GitHub}/Path-Finder`,
   },
 
   {
-    title: 'YelpCamp',
+    name: 'YelpCamp',
     description: 'A campground review app',
-    features: [
-      'create, edit, and delete campgrounds',
-      'create, edit, and delete reviews',
-    ],
-    technologies: ['Node', 'Express', 'MongoDB'],
+    tech: ['Node', 'Express', 'MongoDB'],
     image: 'https://wallpaper.dog/large/20359633.jpg',
     liveLink: `yelpcamp.${Netlify}`,
-    githubLink: `${GitHub}/YelpCamp`,
+    codeLink: `${GitHub}/YelpCamp`,
   },
 ];
 
