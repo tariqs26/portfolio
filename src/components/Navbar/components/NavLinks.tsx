7177be8da4743fa4f9ab768f9d883d05115862d3
Saad T <tariqs26@mcmaster.ca>
2022-12-06 23:20:13 -0500

    simplify formatting of nav links component

--- src/components/Navbar/components/NavLinks.tsx
+++ src/components/Navbar/components/NavLinks.tsx
@@ -3,15 +3,9 @@ import './NavLinks.css';
 export default function NavLinks() {
   return (
     <div className='nav-links'>
-      <a href='#about'>
-        About
-      </a>
-      <a href='#projects'>
-        Projects
-      </a>
-      <a href='#contact'>
-        Contact
-      </a>
+      <a href='#about'>About</a>
+      <a href='#projects'>Projects</a>
+      <a href='#contact'>Contact</a>
     </div>
   );
 }
