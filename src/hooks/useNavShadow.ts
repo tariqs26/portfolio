a328feefc8aa29c89972214d161de9a408f94d2d
Saad T <tariqs26@mcmaster.ca>
2022-12-06 23:01:17 -0500

    simplify navbar scroll functionality

--- src/hooks/useNavShadow.ts
+++ src/hooks/useNavShadow.ts
@@ -1,20 +1,16 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
 export default function useNavShadow() {
-  const [pos, setPos] = useState(0);
   useEffect(() => {
+    const navList = (document.querySelector('nav') as HTMLElement).classList;
     const handleScroll = () => {
-      (pos < 20 || window.scrollY < 20) && setPos(window.scrollY);
+      const contains = navList.contains('nav-shadow');
+      if (window.scrollY > 20) !contains && navList.add('nav-shadow');
+      else contains && navList.remove('nav-shadow');
     };
     window.addEventListener('scroll', handleScroll);
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
-  }, [pos]);
-
-  useEffect(() => {
-    const nav = document.querySelector('nav') as HTMLElement;
-    if (pos > 19) nav.classList.add('nav-shadow');
-    else nav.classList.remove('nav-shadow');
-  }, [pos]);
+  }, []);
 }
