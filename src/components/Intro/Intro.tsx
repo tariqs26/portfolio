32bd8adf5979eaba32cb607da3b162504851f334
Saad T <tariqs26@mcmaster.ca>
2023-01-05 04:33:41 -0500

    remove resume button, add contact and projects link

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -4,36 +4,45 @@ import './Intro.css';
 
 export default function Intro() {
   useEffect(() => {
-    const children = (document.querySelector('.intro') as HTMLElement).children;
+    const children = document.querySelectorAll(
+      '.intro> :not(.content), .intro .content :is(p, div'
+    );
     for (let i = 0; i < children.length; i++)
       (children[i] as HTMLElement).style.animationDelay = `${i * 0.1 + 1.1}s`;
   }, []);
 
   return (
-    <div className='section-container' id='intro'>
+    <div className='section-container'>
       <section className='section'>
         <div className='intro'>
           <div className='intro-greet intro-btn animate'>Hi, I'm</div>
           <h1 className='animate'>Saad Tariq</h1>
           <h2 className='animate'>Web Developer</h2>
-          <div className='intro-links animate'>
-            <IntroLink href='https://github.com/tariqs26' ariaLabel='GitHub'>
-              <GitHubIcon />
-            </IntroLink>
-            <IntroLink
-              href='https://www.linkedin.com/in/saad-tariq-cs/'
-              ariaLabel='Linkedin'
-            >
-              <LinkedInIcon />
-            </IntroLink>
-          </div>
-          <div className='content animate'>
-            <p>
+          <div className='content'>
+            <div className='intro-links animate'>
+              <IntroLink href='https://github.com/tariqs26' ariaLabel='GitHub'>
+                <GitHubIcon />
+              </IntroLink>
+              <IntroLink
+                href='https://www.linkedin.com/in/saad-tariq-cs/'
+                ariaLabel='Linkedin'
+              >
+                <LinkedInIcon />
+              </IntroLink>
+            </div>
+            <p className='animate'>
               Computer science student at McMaster University passionate about
               building things for the web.
             </p>
+            <div className='intro-buttons animate'>
+              <a href='#projects' className='intro-btn'>
+                View My Work
+              </a>
+              <a href='#contact' className='intro-btn'>
+                Contact me
+              </a>
+            </div>
           </div>
-          <div className='intro-btn animate'>View resume</div>
         </div>
       </section>
     </div>
