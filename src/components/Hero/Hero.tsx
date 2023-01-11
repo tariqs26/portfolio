b75d3242c9cb8764d8134415c37a52a40bceda4c
Saad T <tariqs26@mcmaster.ca>
2023-01-11 00:39:42 -0500

    rename all intro components to hero

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -2,10 +2,10 @@ import { useEffect } from 'react';
 import { LinkedInIcon, GitHubIcon } from 'components/Icons';
 import './Hero.css';
 
-export default function Intro() {
+export default function Hero() {
   useEffect(() => {
     const children = document.querySelectorAll(
-      '.intro> :not(.content), .intro .content :is(p, div'
+      '.hero > :not(.content), .hero .content :is(p, div'
     );
     for (let i = 0; i < children.length; i++)
       (children[i] as HTMLElement).style.animationDelay = `${i * 0.1 + 1.1}s`;
@@ -24,31 +24,31 @@ export default function Intro() {
         }}
       ></div>
       <section className='section'>
-        <div className='intro'>
-          <div className='intro-greet intro-btn animate'>Hi, I'm</div>
+        <div className='hero'>
+          <div className='hero-greet hero-btn animate'>Hi, I'm</div>
           <h1 className='animate'>Saad Tariq</h1>
           <h2 className='animate'>Web Developer</h2>
           <div className='content'>
-            <div className='intro-links animate'>
-              <IntroLink href='https://github.com/tariqs26' ariaLabel='GitHub'>
+            <div className='hero-inks animate'>
+              <HeroLink href='https://github.com/tariqs26' ariaLabel='GitHub'>
                 <GitHubIcon />
-              </IntroLink>
-              <IntroLink
+              </HeroLink>
+              <HeroLink
                 href='https://www.linkedin.com/in/saad-tariq-cs/'
                 ariaLabel='Linkedin'
               >
                 <LinkedInIcon />
-              </IntroLink>
+              </HeroLink>
             </div>
             <p className='animate'>
               Computer science student at McMaster University passionate about
               building things for the web.
             </p>
-            <div className='intro-buttons animate'>
-              <a href='#projects' className='intro-btn'>
+            <div className='hero-buttons animate'>
+              <a href='#projects' className='hero-btn'>
                 View my work
               </a>
-              <a href='#contact' className='intro-btn'>
+              <a href='#contact' className='hero-btn'>
                 Contact me
               </a>
             </div>
@@ -60,12 +60,12 @@ export default function Intro() {
   );
 }
 
-type IntroLinkProps = {
+type HeroLinkProps = {
   href: string;
   ariaLabel: string;
   children: React.ReactNode;
 };
-const IntroLink = ({ href, ariaLabel, children }: IntroLinkProps) => (
+const HeroLink = ({ href, ariaLabel, children }: HeroLinkProps) => (
   <a href={href} aria-label={ariaLabel} rel='noreferrer' target='_blank'>
     {children}
   </a>
