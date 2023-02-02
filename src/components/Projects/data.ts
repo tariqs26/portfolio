39866c9d633e1aacafe076769e4ced592ac966de
Saad T <tariqs26@mcmaster.ca>
2023-02-02 08:08:30 -0500

    add yelp-camp image remove vector project

--- src/components/Projects/data.ts
+++ src/components/Projects/data.ts
@@ -3,8 +3,17 @@ const gitHub = (name: string) => `${start}github.com/tariqs26/${name}`;
 const netlify = (name: string) => `${start}${name}.netlify.app`;
 import p1 from 'assets/images/project1.png';
 import p2 from 'assets/images/project2.png';
+import p3 from 'assets/images/project3.png';
 
 const projects = [
+  {
+    name: 'YelpCamp',
+    desc: 'Full-stack web application that allows users to create, edit, and delete campgrounds, and leave reviews and ratings for each campground.',
+    tech: ['React', 'TypeScript', 'Node', 'Express', 'MongoDB'],
+    imgSrc: p3,
+    liveLink: netlify('yelpcamp'),
+    codeLink: gitHub('yelpcamp'),
+  },
   {
     name: 'Neuro',
     desc: 'Trivia app with a time-based scoring system, allowing users to generate questions based on specific topics and difficulty levels.',
@@ -13,21 +22,14 @@ const projects = [
     liveLink: netlify('neuro-trivia'),
     codeLink: gitHub('Neuro'),
   },
-  {
-    name: 'YelpCamp',
-    desc: 'Full-stack web application that allows users to create, edit, and delete campgrounds, and leave reviews and ratings for each campground.',
-    tech: ['React', 'TypeScript', 'Node', 'Express', 'MongoDB'],
-    imgSrc: p1,
-    liveLink: netlify('yelpcamp'),
-    codeLink: gitHub('yelpcamp'),
-  },
-  {
-    name: 'Vector Visualizer',
-    desc: 'An app that allows users to visualize vectors and their operations, using the Elm programming language.',
-    tech: ['HTML', 'CSS', 'Elm'],
-    imgSrc: p2,
-    liveLink: netlify('job-tracker'),
-  },
+  // {
+  //   name: 'Vector Visualizer',
+  //   desc: 'An app that allows users to visualize vectors and their operations, using the Elm programming language.',
+  //   tech: ['HTML', 'CSS', 'Elm'],
+  //   imgSrc: p1,
+  //   liveLink:
+  //     'https://cs1xd3.online/ShowModulePublish?modulePublishId=02b6bf1e-45a3-41ab-91d5-6f6ffe135ef7&fullscreen=true',
+  // },
 ];
 
 export default projects;
