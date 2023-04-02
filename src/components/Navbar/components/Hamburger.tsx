9d8ef1f5764a47b491d062b4deda1b1ac2f660e5
Saad T <tariqs26@mcmaster.ca>
2023-04-02 01:41:58 -0400

    event delegation on parent for anchors

--- src/components/Navbar/components/Hamburger.tsx
+++ src/components/Navbar/components/Hamburger.tsx
@@ -19,16 +19,17 @@ export default function Hamburger({ open, setOpen, menuRef }: HamburgerProps) {
         <div className='bar'></div>
         <div className='bar'></div>
       </div>
-      <div className='menu hide' ref={menuRef} aria-expanded={open}>
-        <a href='#about' onClick={() => setOpen(false)}>
-          About
-        </a>
-        <a href='#projects' onClick={() => setOpen(false)}>
-          Projects
-        </a>
-        <a href='#contact' onClick={() => setOpen(false)}>
-          Contact
-        </a>
+      <div
+        className='menu hide'
+        ref={menuRef}
+        aria-expanded={open}
+        onClick={(e) => {
+          e.target instanceof HTMLAnchorElement && setOpen(false);
+        }}
+      >
+        <a href='#about'>About</a>
+        <a href='#projects'>Projects</a>
+        <a href='#contact'>Contact</a>
       </div>
     </div>
   );
